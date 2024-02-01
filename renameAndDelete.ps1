b589e12e3d5ca63f86f56054442b08c9a9b5ce8d
Duder <soames.alex@gmail.com>
2024-01-31 22:03:21 -0800

    PAIN! Once again
    Only 179 files of 535 are being copied over for some reason

--- renameAndDelete.ps1
+++ renameAndDelete.ps1
@@ -3,11 +3,51 @@ function Convert-EmoDBToRAVDESS{
     param ([string]$emodbFilename)
 
     #Get info from EmoDB file name
-    $speakerNumber = $emodbFilename.Substring(0, 2)
-    $textCode = $emodbFilename.Substring(2, 3)
+    
+	$substringResult = [int]$emodbFilename.Substring(0, 2)
+	#$speakerNumber = $substringResult+50
+	
+	#Odd numbered actors are male, even numbered actors are female
+	if($substringResult -eq 3){
+		#03 - male, 31 years old
+		$speakerNumber = 25
+	}elseif($substringResult -eq 8){
+		#08 - female, 34 years
+		$speakerNumber = 26
+	}elseif($substringResult -eq 9){
+		#09 - female, 21 years
+		$speakerNumber = 28
+	}elseif($substringResult -eq 10){
+		#10 - male, 32 years
+		$speakerNumber = 27
+	}elseif($substringResult -eq 11){
+		#11 - male, 26 years
+		$speakerNumber = 29
+	}elseif($substringResult -eq 12){
+		#12 - male, 30 years
+		$speakerNumber = 31
+	}elseif($substringResult -eq 13){
+		#13 - female, 32 years
+		$speakerNumber = 30
+	}elseif($substringResult -eq 14){
+		#14 - female, 35 years
+		$speakerNumber = 32
+	}elseif($substringResult -eq 15){
+		#15 - male, 25 years
+		$speakerNumber = 33
+	}elseif($substringResult -eq 16){
+		#16 - female, 31 years
+		$speakerNumber = 34
+	}else{
+		$speakerNumber = 99
+	}
+	
+    
+	$textCode = $emodbFilename.Substring(2, 3)
     $emotionCode = $emodbFilename.Substring(5, 1)
-	$intensityCode = 'IC'
-	$statementCode = 'SC'
+	
+	$intensityCode = '01' #Set all to Normal intensity
+	$statementCode = '03' #Set all do a diffrent statement than the 2 in RAVDESS
 	
 	$repCode = $emodbFilename.Substring(6)
 	$repCode = ConvertToNumber($repCode)
@@ -58,14 +98,12 @@ $emodbFiles = Get-ChildItem -Path $emodbFolder -Filter *.wav
 
 foreach ($emodbFile in $emodbFiles) {
     $convertedFilename = Convert-EmoDBToRAVDESS -emodbFilename $emodbFile.BaseName
-    if ($convertedFilename -ne $null) {
-        #Check if the emotion code is 'L' and skip it
-        if ($emodbFile.BaseName.Substring(5, 1) -eq 'L') {
-			#Skip copying
-		}else{
-			$destinationPath = Join-Path -Path $renamedFilesFolder -ChildPath "$convertedFilename.wav"
-			Copy-Item -Path $emodbFile.FullName -Destination $destinationPath
-			Write-Host "EmoDB Filename: $($emodbFile.Name) -> RAVDESS Filename: $($convertedFilename).wav"
-		}
+	#Check if the emotion code is 'L' and skip it
+	if ($emodbFile.BaseName.Substring(5, 1) -eq 'L') {
+		#Skip copying
+	}else{
+		$destinationPath = Join-Path -Path $renamedFilesFolder -ChildPath "$convertedFilename.wav"
+		Copy-Item -Path $emodbFile.FullName -Destination $destinationPath
+		Write-Host "EmoDB Filename: $($emodbFile.Name) -> RAVDESS Filename: $($convertedFilename).wav"
     }
 }
